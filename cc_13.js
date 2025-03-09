95bbadad288f188c9cf0a98010ff36dc3d2173bb
Karim <konanfoi@gmail.com>
2025-03-09 04:48:35 -0400

    Task 1 - Base Structure Created

---
+++ cc_13.js
@@ -0,0 +1,26 @@
+//Task 2 - Adding Employee Cards Dynamically
+const employeeContainer = document.getElementById("employeeContainer");
+
+function addEmployee(name, position) {
+    const employeeCard = document.createElement("div");
+    employeeCard.setAttribute("class", "employee-card");
+    
+    const employeeName = document.createElement("h3");
+    employeeName.textContent = name;
+
+    const employeePosition = document.createElement("p");
+    employeePosition.textContent = position;
+
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    removeButton.addEventListener("click", function(event) {
+        event.stopPropagation();
+        employeeContainer.removeChild(employeeCard);
+    });
+    employeeCard.appendChild(employeeName);
+    employeeCard.appendChild(employeePosition);
+    employeeCard.appendChild(removeButton);
+
+    employeeContainer.appendChild(employeeCard);
+}
+
